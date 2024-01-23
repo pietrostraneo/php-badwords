c7438643813434cb62ae0e00a385119fbb4907c4
pietrostraneo <straneo_p@yahoo.it>
2024-01-23 17:57:34 +0100

    ESERCIZIO CONSEGNATO

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Il primo file dovrà permettere all’utente di inserire i dati e inviare la ric
 Il secondo file riceverà la richiesta ed eseguirà queste operazioni:
 - stampare a schermo il paragrafo e la sua lunghezza
 - stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare
+
+ESERCIZIO CONSEGNATO
---
+++ dashboard.php
@@ -0,0 +1,42 @@
+<?php 
+    $name = $_GET['name'];
+    $surname = $_GET['surname'];
+    $phrase = $_GET['phrase'];
+    $censored = "***";
+    $censoredPhrase = str_replace('dio', $censored, $phrase);
+    $censoredPhrase = str_replace('nazista', $censored, $censoredPhrase);
+    $censoredPhrase = str_replace('fascista', $censored, $censoredPhrase);
+    $censoredPhrase = str_replace('stronzo', $censored, $censoredPhrase)
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Censored BadWords</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row text-center mt-5">
+            <div class="col-12">
+                <h1>Ciao, <?php echo $name.' '.$surname ?>!</h1>
+            </div>
+
+            <div class="col-12 mt-5">
+                <b>Paragrafo senza censure:</b>
+                <p>La lunghezza del tuo paragrafo è: <?php echo strlen($phrase) ?></p>
+                <p><?php echo $phrase ?></p>
+            </div>
+            <div class="col-12 mt-5">
+                <b>Paragrafo con censure:</b>
+                <p>La lunghezza del tuo paragrafo è: <?php echo strlen($phrase) ?></p>
+                <p><?php echo $censoredPhrase ?></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
